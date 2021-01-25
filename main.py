a2b8373b1733dea527e66687ade8dacb473e1437
Francis <flee51449@gmail.com>
2021-01-25 11:38:25 -0800

    Added character

--- main.py
+++ main.py
@@ -3,16 +3,47 @@
 import pygame
 
 #TODO
-# thought about what i wanted to do in my final project
+# 1. thought about what i wanted to do in my final project
+
 
 # ----- CONSTANTS
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
-SKY_BLUE = (95, 165, 228)
+BLUE = (95, 165, 228)
 WIDTH = 800
 HEIGHT = 600
 TITLE = "<Game>"
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.image.load("./image/mario copy.png")
+        self.image = pygame.transform.scale(self.image, (64, 64))
+        self.rect = self.image.get_rect()
+
+        self.vel_x = 0
+        self.vel_y = 0
+
+    def update(self):
+        self.calc_grav()
+
+
+        self.rect.x += self.vel_x
+
+    def calc_grav(self):
+        """ Calculate effect of gravity. """
+        if self.vel_y == 0:
+            self.vel_y = 1
+        else:
+            self.vel_y += .35
+
+        # See if we are on the ground.
+        if self.rect.y >= HEIGHT - self.rect.height and self.vel_y >= 0:
+            self.vel_y = 0
+            self.rect.y = HEIGHT - self.rect.height
 
 
 def main():
@@ -27,6 +58,14 @@ def main():
     done = False
     clock = pygame.time.Clock()
 
+    # group of all the sprites
+    all_sprites = pygame.sprite.Group()
+
+    mario = Player()
+    all_sprites.add(mario)
+
+
+
     # ----- MAIN LOOP
     while not done:
         # -- Event Handler
@@ -35,9 +74,11 @@ def main():
                 done = True
 
         # ----- LOGIC
+        all_sprites.update()
 
         # ----- DRAW
         screen.fill(BLACK)
+        all_sprites.draw(screen)
 
         # ----- UPDATE
         pygame.display.flip()
